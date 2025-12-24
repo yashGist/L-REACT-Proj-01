e2b71d126d7b16283a86d8042c5a5a99d8ebde61
yashGist <ramawatyashdeep@gmail.com>
2025-12-24 08:47:12 +0000

    components separated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
+import CoreConcepts from "./components/CoreConcepts.jsx";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "core", "Crucial"];
-const genRandomNum = (max) => Math.floor(Math.random() * (max + 1));
-
-export function Header() {
-  const description = reactDescriptions[genRandomNum(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,21 @@
+// function CoreConcepts(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+//-----------------------------------------------------------------------------
+// OR USE THIS SHORT ALTERNATIVE for CoreConcepts function, works totally fine!
+//-----------------------------------------------------------------------------
+export default function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
