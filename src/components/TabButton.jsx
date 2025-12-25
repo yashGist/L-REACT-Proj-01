82258e1e26a3f8343e277eb02a9a2184e3f525a0
yashGist <ramawatyashdeep@gmail.com>
2025-12-25 08:11:56 +0000

    Dynamic list functionality added

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton({children}) {
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     <li>
-      <button>{children}</button>
+      <button className={isSelected ? "active" : ""} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
